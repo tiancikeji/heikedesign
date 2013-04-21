e460b2ff10bc507ef894ea40667d890268e7f17e
王雷 <wanglei@chinacache.com>
2013-04-21 14:28:52 +0800

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heikedesign::Application.config.secret_token = '8ae38a4be2aafe69b73b5ca5d36aad23eac40d9f91051fccc28304d37a1da9a90a058ae92749b94a2fcf9b7516e10e56b881748c446125857186390289a84f07'
